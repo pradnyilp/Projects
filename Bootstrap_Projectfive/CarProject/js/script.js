05ff9fdb740a081e65e02ecc86b8d284b1db1b2c
pradnyilp <129146999+pradnyilp@users.noreply.github.com>
2023-05-20 14:28:14 +0530

    five added

---
+++ Bootstrap_Projectfive/CarProject/js/script.js
@@ -0,0 +1,312 @@
+function over(e){
+  document.getElementById('likeimg').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png');
+};
+
+function overtwo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwo').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overthree(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthree').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overfour(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfour').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function leave(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+
+    document.getElementById('likeimg').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+ 
+
+
+
+}; 
+
+
+function leavetwo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwo').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+function leavethree(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthree').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+
+function leavefour(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfour').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+function overr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+   
+    document.getElementById('likeimgg').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+
+   
+
+};
+
+function overtwoo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwoo').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overthreee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreee').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overfourr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourr').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function leavee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+
+    document.getElementById('likeimgg').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+ 
+
+
+
+}; 
+
+
+function leavetwoo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwoo').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+function leavethreee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreee').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+
+function leavefourr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourr').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+function overrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+   
+    document.getElementById('likeimggg').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+
+   
+
+};
+
+function overtwooo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwooo').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overthreeee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreeee').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overfourrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourrr').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function leaveee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+
+    document.getElementById('likeimggg').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+ 
+
+
+
+}; 
+
+
+function leavetwooo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwooo').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+function leavethreeee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreeee').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+
+function leavefourrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourrr').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+
+function overrrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+   
+    document.getElementById('likeimgggg').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+
+   
+
+};
+
+function overtwoooo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwoooo').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overthreeeee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreeeee').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function overfourrrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourrrr').setAttribute('src','Images/BackgroundImges/ecommerce-images15.png')
+  
+
+
+};
+
+
+function leaveeee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+
+    document.getElementById('likeimgggg').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+ 
+
+
+
+}; 
+
+
+function leavetwoooo(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgtwoooo').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+function leavethreeeee(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgthreeeee').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
+
+
+
+function leavefourrrr(e){
+
+    //document.getElementById('#likeimg').style.backgroundColor='black'
+    document.getElementById('likeimgfourrrr').setAttribute('src','Images/BackgroundImges/ecommerce-images14.png')
+   
+
+}; 
